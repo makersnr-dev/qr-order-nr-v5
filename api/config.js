d7ab992325580726f1fe9a64afbe5440b434a7b7
makersnr-dev <cs@make32.com>
2025-11-27 12:02:43 +0900

    Delete api directory

--- api/config.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res){
-  res.json({
-    tossClientKey: process.env.TOSS_CLIENT_KEY || "",
-    origin: req.headers['x-forwarded-host'] ? `https://${req.headers['x-forwarded-host']}` : ''
-  });
-}
