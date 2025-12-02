58e4747eaeeae8217cc430953049630e16060b01
makersnr-dev <cs@make32.com>
2025-12-02 14:22:35 +0900

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,15 @@
-export default async function handler(req, res){
+// /api/config.js
+
+// ✅ 이 파일은 Edge가 아니라 Node.js 런타임으로 실행하게 강제
+export const config = {
+  runtime: 'nodejs',
+};
+
+export default async function handler(req, res) {
   res.json({
     tossClientKey: process.env.TOSS_CLIENT_KEY || "",
-    origin: req.headers['x-forwarded-host'] ? `https://${req.headers['x-forwarded-host']}` : ''
+    origin: req.headers['x-forwarded-host']
+      ? `https://${req.headers['x-forwarded-host']}`
+      : '',
   });
 }
