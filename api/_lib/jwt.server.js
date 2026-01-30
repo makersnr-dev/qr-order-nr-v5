04e782165b9f91903103a797126af41ee740abf3
makersnr-dev <cs@make32.com>
2026-01-30 10:07:14 +0900

    Update jwt.server.js

--- api/_lib/jwt.server.js
+++ api/_lib/jwt.server.js
@@ -1,9 +1,9 @@
-/*/api/_lib/jwt.server.js*/
-import { signJWT } from "../../src/shared/jwt.js";  // 상대경로로 수정
+// /api/_lib/jwt.server.js
+import { signJWT, verifyJWT } from "../../src/shared/jwt.js";  // 상대경로로 수정
 
 export function verifyJWT(token, secret) {
   return new Promise((resolve, reject) => {
-    verifyJWTFromEdge(token, secret)
+    verifyJWT(token, secret)  // 수정된 부분: verifyJWT로 변경
       .then((decoded) => resolve(decoded))
       .catch((err) => reject(err));
   });
