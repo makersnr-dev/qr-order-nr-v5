5b90cf75451e5ae7a669e1630d85af24bdc66516
makersnr-dev <cs@make32.com>
2026-01-30 09:48:15 +0900

    Update jwt.server.js

--- api/_lib/jwt.server.js
+++ api/_lib/jwt.server.js
@@ -1,4 +1,4 @@
-import { verifyJWT as verifyJWTFromEdge } from "/src/shared/jwt.js";
+import { signJWT } from "../shared/jwt.js";  // 상대경로로 수정
 
 export function verifyJWT(token, secret) {
   return new Promise((resolve, reject) => {
