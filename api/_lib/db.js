7e9fd565dfb5bfa1711f2f0e7803cbbd363f06b5
makersnr-dev <cs@make32.com>
2026-03-05 14:25:28 +0900

    Update db.js

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -3,15 +3,14 @@ const { Pool } = pkg;
 
 if (!process.env.DATABASE_URL) {
   console.error("❌ DATABASE_URL is not set");
-  throw new Error("❌ DATABASE_URL is not set");
 }
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 15,                     // Vercel 환경에 맞게 적정 수준 유지
-  idleTimeoutMillis: 30000,    // 🚀 연결 유지 시간 1초 -> 30초로 증가 (매번 재연결하는 부하 방지)
-  connectionTimeoutMillis: 10000, // 🚀 접속 대기 시간 2초 -> 10초로 증가 (DB가 깨어날 충분한 시간 확보)
-  ssl: true
+  connectionString: process.env.DATABASE_URL || "postgres://dummy:dummy@dummy:5432/dummy",
+  max: 15,                     // Vercel 환경에 맞게 적정 수준 유지
+  idleTimeoutMillis: 30000,    // 🚀 연결 유지 시간 1초 -> 30초로 증가 (매번 재연결하는 부하 방지)
+  connectionTimeoutMillis: 10000, // 🚀 접속 대기 시간 2초 -> 10초로 증가 (DB가 깨어날 충분한 시간 확보)
+  ssl: true
 });
 
 // ✅ 연결 테스트 추가
